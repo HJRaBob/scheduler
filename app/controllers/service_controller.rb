bafb0518360f0879b6ce81928f4334759130e274
HJRaBob <kidma222@naver.com>
2018-01-17 14:05:22 +0000

    setting function

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -1,15 +1,30 @@
 class ServiceController < ApplicationController
+    before_action :authenticate_user!
+    
     def main
         @user = current_user
         @service = Service.where(:user_id => current_user.id).order("sequence ASC")
     end
 
     def setting
-        
+        @user = current_user
+        @service = Service.where(:user_id => current_user.id).order("sequence ASC")
     end
 
     def save
-    
+        User.find(current_user.id).update(
+            :service_number => params[:service_number],
+            :music_site => params[:music_site],
+            :fortune_year => params[:fortune_year]
+        )
+        
+        services = Service.where(:user_id => current_user.id).order("sequence ASC")
+        8.times do |i|
+            services[i].update(:id_service => params[:"widget_#{i+1}"])
+        end
+
+
+        redirect_to "/"
     end
 
     def allocate
